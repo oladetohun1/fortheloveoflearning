9154b2863f73b36a59b3ecf729b7d2b03b8cb2fd
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-08-22 04:36:10 +0100

    no5

---
+++ Java_script/05.js
@@ -0,0 +1,19 @@
+function hello(){
+    console.log("hello world!")
+}
+function helloYou(name){
+    console.log("hello " + name);
+}
+function addNUm(num1,num2){
+    console.log(num1+num2)
+}
+// default parameter
+function helloSomeone(name="Frankie"){
+    console.log("Hello " +name)
+} 
+function fivestimes(numinput){
+    var result = numinput*5
+    return result
+}
+// scope
+ 
