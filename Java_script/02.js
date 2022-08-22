ecf129cf6243d83e2524322dd6bfdaa37477c76c
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-08-22 04:34:45 +0100

    second js

---
+++ Java_script/02.js
@@ -0,0 +1,13 @@
+var hot = false
+var temp = prompt("What temperature is it:")
+if (temp>60) {
+    console.log("temperature is greater than 60 degree " )
+    alert("Temperature is greater than 60 degree" )
+    
+}
+else if (temp<60) {
+    alert("Temperature is less than 60 degree" )
+}
+else{
+    alert("Temperature is " + temp)
+}
