52e767a1c2bbee6a98dc464f73092e43ecb7d0bb
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-08-22 04:35:11 +0100

    thied js

---
+++ Java_script/03.js
@@ -0,0 +1,14 @@
+// var x = 0
+// while(x<5){
+//     console.log("X is currently "+ x)
+//     console.log("X is still less than 5, adding one to x")
+//     x = x+1
+// }
+
+var x = 1
+while (x<11){
+    if (x%2 === 0){
+        console.log(x)
+    }
+x=x+1
+}
