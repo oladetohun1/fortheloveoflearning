ad8118f774a3e8ac710a3e93841e1c4266628e73
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-08-22 04:35:48 +0100

    no4

---
+++ Java_script/04.js
@@ -0,0 +1,8 @@
+// FOR LOOPS
+for(var i = 0; i<5; i++){
+    console.log("Number is " + i)
+}
+// another example
+var word = "ABCDEFGHIJK"
+for (var i = 0 ; i < word.length; i++)
+console.log(word[i])
